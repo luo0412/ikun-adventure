e2e50353adb406ad7d3a4e05678afdacfca8262b
1095847440@qq.com <zhijun.wu@informa.com>
2026-08-13 18:27:44 +0800

    adjust

--- liang-src/liang-intensity-calibrator-main/src/worker.ts
+++
@@ -1,9 +0,0 @@
-interface AssetsBinding {
-  fetch(request: Request): Promise<Response>;
-}
-
-export default {
-  fetch(request: Request, env: { ASSETS: AssetsBinding }): Promise<Response> {
-    return env.ASSETS.fetch(request);
-  },
-};
--- liang-src/liang-intensity-calibrator-main/vite.pages.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: "/liang-intensity-calibrator/",
-  build: {
-    outDir: "dist-pages",
-  },
-});
